3297cc039e28852ed5383ace638a5623e062e6e3
thomas-ring <thomasjring@me.com>
2016-12-09 04:37:27 -0500

    Add tests sub module for the expression module

---
+++ src/parser/expression/tests.rs
@@ -0,0 +1,6 @@
+/// parser/expression/tests.rs
+///
+/// This file contains unit tests for the expression parser for correct parsing as well
+/// as correct assembly code generation from that parsing.
+
+use super::*;
